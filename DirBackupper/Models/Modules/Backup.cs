56a40a80d6c567e8faf0cddc02abe1a587d7f5aa
Yakumo Karasuma <yakumo.karasuma@gmail.com>
2021-07-04 11:48:05 +0900

    バックアップ操作を更に非同期化

--- DirBackupper/Models/Modules/Backup.cs
+++ DirBackupper/Models/Modules/Backup.cs
@@ -21,35 +21,6 @@ namespace DirBackupper.Models.Modules
 		private void ReportInfo(IProgress<ProgressInfo> progress, float ratio, string message)
 			=> progress.Report( new ProgressInfo( ratio, message ) );
 
-		private async Task CopyFileStrictlyAsync(string source, string destination)
-		{
-			using ( var src = new FileStream( source, FileMode.Open, FileAccess.Read ) )
-			using ( var dst = new FileStream( destination, FileMode.OpenOrCreate, FileAccess.Write ) )
-			{
-				var orig = new byte[src.Length];
-				var bytesFrom = 0;
-				var bytesTo = (int)src.Length;
-				while ( bytesTo > 0 )
-				{
-					var count = await src.ReadAsync( orig, bytesFrom, bytesTo );
-					if ( count == 0 )
-						break;
-					bytesFrom += count;
-					bytesTo -= count;
-				}
-				await dst.WriteAsync( orig, 0, orig.Length );
-			}
-		}
-
-		private void CreateDirectoryRecursive(string path)
-		{
-			var dir = Path.GetDirectoryName( path );
-			if ( !Directory.Exists( dir ) )
-				CreateDirectoryRecursive( dir.Substring( 0, dir.LastIndexOf( @"\" ) ) );
-
-			Directory.CreateDirectory( dir );
-		}
-
 		public async Task<TaskDoneStatus> Execute(IProgress<ProgressInfo> progress, string sourceDir, string destDir)
 		{
 			var message = new Func<string, string>( msg =>
@@ -78,7 +49,7 @@ namespace DirBackupper.Models.Modules
 					var proceededFileCount = 0;
 					var allFiles = int.MaxValue;
 					var currentRatio = new Func<float>( () => allFiles > 0 ? (float)proceededFileCount / (float)allFiles : 0f );
-					await Task.Run( async () =>
+					await Task.Run( () =>
 					 {
 						 // Count all files
 						 var srcpathes = new[]
@@ -93,36 +64,34 @@ namespace DirBackupper.Models.Modules
 						 if ( !Directory.Exists( destDir ) )
 							 Directory.CreateDirectory( destDir );
 
-						 // Copy files
-						 foreach ( var src in srcpathes )
+						 var parallelOptions = new ParallelOptions()
 						 {
-							 var isDir = isDirectory( src );
-							 var realsrc = src.Substring( 3 );
-							 var dst = Path.Combine( Path.GetDirectoryName( destDir ), realsrc.Substring( realsrc.IndexOf( sourceDir ) + sourceDir.Length ) ) + ( isDir ? "\\" : "" );
+							 CancellationToken = _cancellation.Token,
+							 MaxDegreeOfParallelism = Environment.ProcessorCount
+						 };
+						 // Copy directories
+						 Parallel.ForEach( Directory.GetDirectories( sourceDir, "*", SearchOption.AllDirectories ).Select( p => p + "\\" ), parallelOptions, src =>
+						  {
+							  var dest = Path.Combine( Path.GetDirectoryName( destDir ), src.Substring( src.IndexOf( sourceDir ) + sourceDir.Length ) );
+
+							  if ( !Directory.Exists( Path.GetDirectoryName( dest ) ) )
+								  Tools.CreateDirectoryRecursive( dest );
+
+							  proceededFileCount++;
+							  ReportInfo( progress, currentRatio(), "Copied: " + src );
+						  } );
+
+						 // Copy files
+						 Parallel.ForEach( Directory.GetFiles( sourceDir, "*", SearchOption.AllDirectories ), parallelOptions, async src =>
+						 {
+							 var dest = Path.Combine( Path.GetDirectoryName( destDir ), src.Substring( src.IndexOf( sourceDir ) + sourceDir.Length ) );
+							 var moved = AllowOverwrite || !File.Exists( src );
+							 if ( moved )
+								 await Tools.CopyFileStrictlyAsync( src, dest );
 
 							 proceededFileCount++;
-
-							 // Check and create directory
-							 if ( isDir )
-							 {
-								 if ( !Directory.Exists( Path.GetDirectoryName( dst ) ) )
-								 {
-									 CreateDirectoryRecursive( dst );
-								 }
-								 continue;
-							 }
-
-							 // Copy current file
-							 if ( AllowOverwrite || !File.Exists( realsrc ) )
-							 {
-								 await CopyFileStrictlyAsync( realsrc, dst );
-								 ReportInfo( progress, currentRatio(), "Copied: " + realsrc );
-							 }
-							 else
-							 {
-								 ReportInfo( progress, currentRatio(), "Hold: " + dst );
-							 }
-						 }
+							 ReportInfo( progress, currentRatio(), moved ? $"Copied: {src}" : $"Hold: {dest}" );
+						 } );
 					 }, _cancellation.Token ).ConfigureAwait( false );
 				}
 				catch ( OperationCanceledException ocex )
