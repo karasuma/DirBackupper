9d78746d1dfd3d172ff22755ff2930248e630f6c
Yakumo Karasuma <yakumo.karasuma@gmail.com>
2021-07-04 18:54:22 +0900

    Restoreの実装とテスト、共通モジュールの分離

---
+++ DirBackupper/Models/Modules/Restore.cs
@@ -0,0 +1,111 @@
+﻿using DirBackupper.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DirBackupper.Models.Modules
+{
+	public class Restore : IBackupTask
+	{
+		public Restore(bool overwrite, string tempPath = "")
+		{
+			AllowOverwrite = overwrite;
+			TemporaryPath = string.IsNullOrEmpty( tempPath ) ? Path.GetFullPath( ".\\Stockpile\\" ) : tempPath;
+		}
+
+		private CancellationTokenSource _cancellation = null;
+
+		public bool AllowOverwrite { get; set; } = false;
+
+		public string TemporaryPath { get; set; } = string.Empty;
+
+		public bool IsRestoring { get; private set; } = false;
+
+		public void CancelExecute()
+		{
+			if ( IsRestoring )
+				throw new InvalidOperationException( "Restore operation can not cancel!" );
+			_cancellation?.Cancel();
+		}
+
+		public async Task<TaskDoneStatus> BackupToTemporary(string sourceDir)
+		{
+			try
+			{
+				return await Execute( null, sourceDir, TemporaryPath );
+			}
+			catch ( OperationCanceledException )
+			{
+				return TaskDoneStatus.Cancelled;
+			}
+			catch ( Exception unknownex )
+			{
+				Logger.Log( $"Temporary files creation failed.{Tools.NewLine}{unknownex}", Logger.LogStates.Error );
+				return TaskDoneStatus.Failed;
+			}
+		}
+
+		public async Task<TaskDoneStatus> RestoreExecute(string restoreDir) => await Execute( null, TemporaryPath, restoreDir );
+
+		public async Task<TaskDoneStatus> Execute(IProgress<ProgressInfo> none, string tempDir, string restoreDir)
+		{
+			IsRestoring = true;
+			using ( _cancellation = new CancellationTokenSource() )
+			{
+				try
+				{
+					await Task.Run( () =>
+					{
+						// Create destination directory
+						if ( !Directory.Exists( restoreDir ) )
+							Directory.CreateDirectory( restoreDir );
+
+						// Create directories if necessary
+						foreach ( var src in Directory.GetDirectories( tempDir, "*", SearchOption.AllDirectories ).Select( p => p + "\\" ) )
+						{
+							var dest = Path.Combine( Path.GetDirectoryName( restoreDir ), src.Substring( src.IndexOf( tempDir ) + tempDir.Length ) );
+
+							if ( !Directory.Exists( Path.GetDirectoryName( dest ) ) )
+								Tools.CreateDirectoryRecursive( dest );
+						}
+
+						// Copy files
+						var parallelOptions = new ParallelOptions()
+						{
+							CancellationToken = _cancellation.Token,
+							MaxDegreeOfParallelism = Environment.ProcessorCount
+						};
+						Parallel.ForEach( Directory.GetFiles( tempDir, "*", SearchOption.AllDirectories ), parallelOptions, async src =>
+							{
+								var dest = Path.Combine( Path.GetDirectoryName( restoreDir ), src.Substring( src.IndexOf( tempDir ) + tempDir.Length ) );
+
+								if ( AllowOverwrite || !File.Exists( src ) )
+									await Tools.CopyFileStrictlyAsync( src, dest );
+							} );
+					}, _cancellation.Token ).ConfigureAwait( false );
+				}
+				catch ( OperationCanceledException )
+				{
+					throw;
+				}
+				catch ( Exception unknownex )
+				{
+					Logger.Log( $"Restore operation failed.{Tools.NewLine}{unknownex}", Logger.LogStates.Error );
+					return TaskDoneStatus.Failed;
+				}
+				finally
+				{
+					IsRestoring = false;
+				}
+			}
+
+			Logger.Log( $"Restore operation completed successfully!", Logger.LogStates.Info );
+			return TaskDoneStatus.Completed;
+		}
+	}
+}
--- DirBackupperUnitTest/Models/BackupUnitTest.cs
+++ DirBackupperUnitTest/Models/BackupUnitTest.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Linq;
-using System.Collections;
-using System.Collections.Generic;
+﻿using DirBackupper.Models.Modules;
+using DirBackupperUnitTest.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using DirBackupper.Models.Modules;
-using DirBackupper.Utils;
-using System.IO;
+using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
@@ -16,56 +12,6 @@ namespace DirBackupperUnitTest
 	{
 		private string SourceDir { get => @".\Source\"; }
 		private string DestDir { get => @".\Destination\"; }
-
-		private IEnumerable<string> SubFileDirs(string basedir)
-		{
-			if ( basedir.Last() != '\\' )
-				basedir += '\\';
-			return new[]
-			{
-				"test01.file",
-				@"dir01\",
-				@"dir01\test02.dat",
-				@"dir01\test03",
-				@"dir01\dir02\",
-				@"dir01\dir02\test04",
-				@"dir01\dir03\"
-			}.Select( after => basedir + after );
-		}
-		private bool IsDirectory(string path)
-			=> path.IndexOfAny( Path.GetInvalidPathChars() ) == -1 ? false : File.GetAttributes( path ).HasFlag( FileAttributes.Directory );
-
-		private void CreateDir(string dir)
-		{
-			var sources = SubFileDirs( dir );
-			foreach ( var d in sources.Where( f => f.Last() == '\\' ) )
-			{
-				var currdir = Path.GetDirectoryName( d );
-				if ( !Directory.Exists( currdir ) )
-					Directory.CreateDirectory( currdir );
-			}
-			foreach ( var f in sources.Where( f => f.Last() != '\\' ) )
-			{
-				if ( !File.Exists( f ) )
-					using ( File.Create( f ) ) { }
-			}
-		}
-
-		private void RemoveDir(string dir)
-		{
-			var sources = SubFileDirs( dir );
-			foreach ( var f in sources.Where( f => f.Last() != '\\' ) )
-			{
-				if ( File.Exists( f ) )
-					File.Delete( f );
-			}
-			foreach ( var d in sources.Where( f => f.Last() == '\\' ) )
-			{
-				var currdir = Path.GetDirectoryName( d );
-				if ( Directory.Exists( currdir ) )
-					Directory.Delete( currdir, true );
-			}
-		}
 
 		private async Task<TaskDoneStatus> MainExecuteProcess()
 		{
@@ -86,9 +32,9 @@ namespace DirBackupperUnitTest
 		public async Task ExecuteTestAsync()
 		{
 			// Arrange
-			RemoveDir( DestDir );
-			RemoveDir( SourceDir );
-			CreateDir( SourceDir );
+			Tools.RemoveDir( DestDir );
+			Tools.RemoveDir( SourceDir );
+			Tools.CreateDir( SourceDir );
 
 			// Act
 			var result = await MainExecuteProcess();
@@ -101,9 +47,9 @@ namespace DirBackupperUnitTest
 		public async Task ExecuteAndOverwriteTestAsync()
 		{
 			// Arrange
-			RemoveDir( DestDir );
-			RemoveDir( SourceDir );
-			CreateDir( SourceDir );
+			Tools.RemoveDir( DestDir );
+			Tools.RemoveDir( SourceDir );
+			Tools.CreateDir( SourceDir );
 
 			// Act
 			var result1 = await MainExecuteProcess();
